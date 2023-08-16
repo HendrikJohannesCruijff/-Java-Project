e452f07b558304fedfe382cefdbdc1cb0162feb2
Red <qjtjzj2733@naver.com>
2023-08-16 20:04:24 +0900

    test

---
+++ Daitso/Mypage/Main.java
@@ -0,0 +1,9 @@
+package Mypage;
+
+public class Main {
+    public static void main(String[] args) {
+        MyPageList myPageList = new MyPageList();
+
+        myPageList.myPageScreen();
+    }
+}
